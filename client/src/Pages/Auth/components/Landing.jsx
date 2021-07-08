fdec51b7a4f2fb71c7125d87ad3082605d15ad64
Billy <billyjcatchpole@gmail.com>
2021-07-08 19:07:45 +0100

    feat: more work on authentication, ui built, need to make it dynamic

--- client/src/Pages/Auth/components/Landing.jsx
+++ client/src/Pages/Auth/components/Landing.jsx
@@ -1,23 +1,58 @@
-import React from "react";
-import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import Background from "../../../Assets/Auth-background.svg";
 
 const Landing = () => {
+  const [currentAuth, setCurrentAuth] = useState("LOGIN");
+  const [inputs, setInputs] = useState([
+    { name: "Email address", value: "" },
+    { name: "Password", value: "", type: "password" },
+  ]);
+
+  const handleToggleAuth = () => {
+    // switch(currentAuth){
+    //   case
+    // }
+  };
   return (
     <div className="w-full h-full flex flex-col">
-      <div className="flex flex-grow"></div>
+      <div className="flex flex-grow items-start">
+        <img src={Background} className="p-10" alt="logo" />
+      </div>
 
-      <div className="h-28 px-5 flex flex-col">
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          className="h-12 shadow"
-        >
-          Log In
-        </Button>
-        <span className="mt-3 text-sm self-center">
-          Need to register? Tap <b>here</b>
-        </span>
+      <div className="px-5 flex flex-col h-4/6">
+        {inputs.map(({ name, value, type }) => (
+          <div className="mb-2">
+            <TextField
+              label={name}
+              variant="outlined"
+              fullWidth
+              value={value}
+              type={type ? type : "text"}
+              onChange={({ target }, name) => {
+                console.log(target.value);
+                console.log(name);
+              }}
+            />
+          </div>
+        ))}
+        <div className="mt-8 flex flex-col items-center">
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            className="h-12 shadow"
+          >
+            Log In
+          </Button>
+          <button
+            className="mt-3 text-sm"
+            type="button"
+            onClick={handleToggleAuth}
+          >
+            Need to register? Tap <b>here</b>
+          </button>
+        </div>
       </div>
     </div>
   );
