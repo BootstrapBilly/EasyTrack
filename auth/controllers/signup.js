750db1c1f22f69c2588ff940d5414381749c567a
Billy <billyjcatchpole@gmail.com>
2021-07-10 18:05:58 +0100

    feat: added form component lib and finished auth layout

--- auth/controllers/signup.js
+++ auth/controllers/signup.js
@@ -6,30 +6,30 @@ const bcrypt = require('bcrypt');
 const signup = async (req, res) => {
     const { email, username, password } = req.body;
 
-    try {    
+    try {
         // sanitzation
         const { malformedReqBody, sanitizedData } = sanitize([email, username, password]);
-        if(malformedReqBody) return attackDetectedResponse(res); // if the req body is malformed, do not proceed any further
+        if (malformedReqBody) return attackDetectedResponse(res); // if the req body is malformed, do not proceed any further
 
         const [sanitzedEmail, sanitzedUsername, sanitzedPassword] = sanitizedData;
-    
+
         // email validation
         const invalidEmail = validateEmailAddress(sanitzedEmail); // check to see if the email is valid
-        if(invalidEmail) return userErrorResponse(res, "Enter a valid email address", { field: "email" }); // if the email is not valid, return an error response
+        if (invalidEmail) return userErrorResponse(res, "Enter a valid email address", { field: "email" }); // if the email is not valid, return an error response
 
-        const emailInUse = await User.findOne({email: sanitzedEmail}); // check if that email is already in the database
-        if(emailInUse) return userErrorResponse(res, "Email in use, try another one", { field: "email" }); // if the email is in use, send an error response
+        const emailInUse = await User.findOne({ email: sanitzedEmail }); // check if that email is already in the database
+        if (emailInUse) return userErrorResponse(res, "Email in use, try another one", { field: "email" }); // if the email is in use, send an error response
 
         // username validation
         const usernameTooShort = checkRequiredValue({ value: sanitzedUsername }, { length: 3 }); // ensure the username is at least 3 characters
-        if(usernameTooShort) return userErrorResponse(res, "Username must be at least 3 characters", { field: "username" }); // if it is too short, send an error response
+        if (usernameTooShort) return userErrorResponse(res, "Username must be at least 3 characters", { field: "username" }); // if it is too short, send an error response
 
         // password validation
-        const passwordTooShort = checkRequiredValue({ value: sanitzedPassword }, { length: 8}); // ensure the password is at least 8 characters
-        if(passwordTooShort) return userErrorResponse(res, "Password must be at least 8 characters", { field: "password" }); // if it is too short, send an error response
+        const passwordTooShort = checkRequiredValue({ value: sanitzedPassword }, { length: 8 }); // ensure the password is at least 8 characters
+        if (passwordTooShort) return userErrorResponse(res, "Password must be at least 8 characters", { field: "password" }); // if it is too short, send an error response
 
         const commonPasswordFound = commonPassword(sanitzedPassword);
-        if(commonPasswordFound) return userErrorResponse(res, "Common password detected, please use a better one", { field: "password" }); // if it is too short, send an error response
+        if (commonPasswordFound) return userErrorResponse(res, "Common password detected, please use a better one", { field: "password" }); // if it is too short, send an error response
 
         // password hashing
         const hashedPassword = await bcrypt.hash(sanitzedPassword, 12) // hash the password with a salt of 12
@@ -46,7 +46,7 @@ const signup = async (req, res) => {
             id: user._id, // with the user id
         });
 
-    } 
+    }
     catch (error) { // if something fails in the try block
         return serverErrorResponse(res); // return a server ever
     }
